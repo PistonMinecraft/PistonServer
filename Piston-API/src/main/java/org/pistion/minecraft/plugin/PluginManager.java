cd5ed521dfdf48d877691654b0cebb15c9bbb27a
CanYingisme-Git <3192799549@qq.com>
2021-01-23 20:49:03 +0800

    Test commit

--- Piston-API/src/main/java/org/pistion/minecraft/plugin/PluginManager.java
+++ Piston-API/src/main/java/org/pistion/minecraft/plugin/PluginManager.java
@@ -1,14 +1,26 @@
 package org.pistion.minecraft.plugin;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class PluginManager {
     public static boolean loadPistonPlugin(String mainClass ){
         //加载Piston插件
         //此函数不会判断主类是否继承PistonPlugin
+        Class clazz = null;
+        PistonPlugin plugin = null;
         try {
-            Class clazz = Class.forName(mainClass);
-        } catch (ClassNotFoundException e) {
+            clazz = Class.forName(mainClass);
+            plugin = (PistonPlugin) clazz.newInstance();
+            Method method = clazz.getMethod("onEnable",String.class);
+            method.invoke(plugin);
+        } catch (ClassNotFoundException
+                | IllegalAccessException
+                | InstantiationException
+                | NoSuchMethodException
+                | InvocationTargetException e) {
             e.printStackTrace();
         }
-        return true;
+        return false;
     }
 }
