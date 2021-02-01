3273bb323e5bf4160be960e8227ee8dda13af4ba
XiaoPangxie732 <47449269+XiaoPangxie732@users.noreply.github.com>
2021-02-01 22:34:43 +0800

    update build script and logging

--- Piston-Server/src/main/java/org/pistonmc/server/main/PistonMain.java
+++ Piston-Server/src/main/java/org/pistonmc/server/main/PistonMain.java
@@ -1,7 +1,13 @@
 package org.pistonmc.server.main;
 
+import joptsimple.OptionParser;
+import net.minecraft.server.Main;
+
 public class PistonMain {
     public static void main(String[] args) {
-        ;
+        OptionParser parser = new OptionParser();
+        Main.main(null);
+    }
+    private static void initLog4jProperties() {
     }
 }
--- buildSrc/src/main/groovy/org/pistonmc/build/task/GenProcessedVanillaServerJarTask.groovy
+++ buildSrc/src/main/groovy/org/pistonmc/build/task/GenProcessedVanillaServerJarTask.groovy
@@ -1,6 +1,6 @@
 package org.pistonmc.build.task
 
-import cn.maxpixel.mcdecompiler.Info
+
 import cn.maxpixel.mcdecompiler.Properties
 import cn.maxpixel.mcdecompiler.deobfuscator.ProguardDeobfuscator
 import cn.maxpixel.mcdecompiler.util.FileUtil
@@ -9,6 +9,7 @@ import cn.maxpixel.mcdecompiler.util.VersionManifest
 import com.google.gson.JsonObject
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 import java.nio.file.Files
@@ -17,19 +18,22 @@ import java.nio.file.StandardCopyOption
 
 class GenProcessedVanillaServerJarTask extends DefaultTask {
     private final Path PISTON_DIR = project.buildDir.toPath().resolve('pistonmc')
-
     @Input
     boolean forceProcess
 
+    @OutputFile
+    Path getOutputPath() {
+        return PISTON_DIR.resolve('server_processed.jar')
+    }
+
     @TaskAction
-    File execute() {
+    void execute() {
         String mcVersion = project.rootProject.ext.MC_VERSION
         Properties.put(Properties.Key.TEMP_DIR, PISTON_DIR.resolve('md-temp'))
         Path inputPath = PISTON_DIR.resolve('server.jar')
-        Path mappingPath = PISTON_DIR.resolve(Properties.getDownloadedProguardMappingPath(mcVersion, Info.SideType.SERVER))
+        Path mappingPath = PISTON_DIR.resolve('mappings.txt')
         FileUtil.ensureDirectoryExist(mappingPath.getParent())
-        Path outputPath = PISTON_DIR.resolve('server_processed.jar')
-        if(!forceProcess && Files.exists(outputPath)) return outputPath.toFile()
+        if(!forceProcess && Files.exists(outputPath)) return
         checkFile(inputPath, VersionManifest.getVersion(mcVersion).get('downloads').asJsonObject.get('server').asJsonObject)
         checkFile(mappingPath, VersionManifest.getVersion(mcVersion).get('downloads').asJsonObject.get('server_mappings').asJsonObject)
         logger.info('Trying processing jar...')
@@ -37,24 +41,24 @@ class GenProcessedVanillaServerJarTask extends DefaultTask {
             Files.deleteIfExists(outputPath)
         } catch(IOException ignored) {
             logger.info('Process failed. Maybe your IDE locked the previously processed jar')
-            return outputPath.toFile()
+            return
         }
+        FileUtil.deleteDirectory(Properties.get(Properties.Key.TEMP_DIR))
         ProguardDeobfuscator deobfuscator = new ProguardDeobfuscator(mappingPath.toString())
         deobfuscator.deobfuscate(inputPath, outputPath)
         logger.info('Processed complete.')
-        return outputPath.toFile()
     }
     void checkFile(Path inputPath, JsonObject obj) {
         if(Files.exists(inputPath)) {
             byte[] b = Files.readAllBytes(inputPath)
             if(!(b.digest('SHA-1') == obj.get('sha1').asString && b.length == obj.get('size').asInt)) {
-                logger.error('Unmapped jar doesn\'t match hash or size, redownloading...')
+                logger.error('File doesn\'t match hash or size, redownloading...')
                 Files.delete(inputPath)
             }
         }
         if(Files.notExists(inputPath)) {
             NetworkUtil.newBuilder(obj.get("url").getAsString()).connect().withCloseable {
-                logger.info('Downloading jar...')
+                logger.info('Downloading...')
                 Files.copy(it.asStream(), inputPath, StandardCopyOption.REPLACE_EXISTING);
             }
         }
